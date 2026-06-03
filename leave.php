d67ecbe1b858b97fbbc0729b2d41301baa51b7fa
Surajshahi2020 <suraj.171245@ncit.edu.np>
2026-06-03 10:49:31 +0545

    self hosting or local hosting

--- leave.php
+++ leave.php
@@ -838,8 +838,12 @@ ob_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Leave Management System</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"> -->
+    <link href="css/fontawesome.css" rel="stylesheet">
+
     <style>
         .status-pending { background-color: #fff3cd; color: #856404; }
         .status-verified { background-color: #cce5ff; color: #004085; }
